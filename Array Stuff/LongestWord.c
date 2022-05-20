d422eaf43711fc5e8f297084c6cbb029e8af5600
Lemintex <aaron_nobile@hotmail.co.uk>
2022-05-20 13:16:44 +0100

    added method prototype

--- Array Stuff/LongestWord.c
+++ Array Stuff/LongestWord.c
@@ -5,6 +5,8 @@
 
 #define MAX_WORD_LENGTH 1000
 
+void copyWord(char to[], char wordToCopy[], int wordLength);
+
 /*
     prints the longest word in a sentence
     bug if final word is the biggest and followed by '\n'
