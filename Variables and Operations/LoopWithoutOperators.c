7401c29ca13f2e6222542a9b528d293fcbd94eb0
Lemintex <aaron_nobile@hotmail.co.uk>
2022-05-24 03:01:30 +0100

    loop without operators

---
+++ Variables and Operations/LoopWithoutOperators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+    the book wants me too not use || or &&, so here it is
+*/
+const int LIMIT = 1000;
+int main() {
+    char string[LIMIT];
+    char c;
+    for (int i = 0; i < LIMIT - 1; i++) {
+        if ((c=getchar())=='\n') {
+            break;
+        }
+        string[i] = c;
+    }
+    printf("String:%s", string);
+}
