faf952f6a2739a1d0d30d54dfd3913ccc7a8917b
Lemintex <57079368+Lemintex@users.noreply.github.com>
2022-08-27 02:55:50 +0100

    Added v1 of reverse polish notation calculator

---
+++ Calculator/Calculator.c
@@ -0,0 +1,135 @@
+/*
+    a basic single integer calculator that uses reverse polish notation and returns the result
+    reverse polish notation means operands precede the operator, so:
+    (5 + 1) / (2 + 1) = 2
+    is written as:
+    5 1 + 2 1 + / = : resulting in 2
+
+    or ((5 + 1) / 2) + 1 = 4
+    is written as
+    5 1 + 2 / 1 + = : resulting in 4
+
+    this is a first version so doesn't handle input errors, integers > 9, or decimal inputs
+*/
+
+#include <stdio.h>
+#define MAX_STACK_SIZE 100
+
+// stack variables
+int stack_index = 0;
+double stack[MAX_STACK_SIZE];
+
+// stack functions
+double push(double n);
+double pop();
+
+// calculator logic
+double result;
+void nextChar(int c);
+int numericCheck(int c);
+
+// pushes n to the top of the stack
+double push(double n)
+{
+    if (stack_index < MAX_STACK_SIZE)
+    {
+        stack[stack_index] = n;
+        stack_index++;
+        return n;
+    }
+    else
+    {
+        printf("Stack full");
+        return 0;
+    }
+}
+
+// pops and returns the top of the stack
+double pop()
+{
+    if (stack_index > 0)
+    {
+        stack_index--;
+        return stack[stack_index];
+    }
+    else
+    {  
+        printf("Stack empty");
+        return 0;
+    }
+}
+
+// resets the stack index
+void empty()
+{
+    stack_index = 0;
+}
+
+int main()
+{
+    int c;
+    while ((c = getchar()) !=  '\n')
+    {
+        if (!numericCheck(c) && c != ' ')
+        {
+            nextChar(c);
+        }
+    }
+    printf("Result: %.2f.", result);
+}
+
+// checks for an operator
+// where operand order doesnt matter I decided not to write in shorthand form to improve readability
+void nextChar(int c)
+{
+    double operandA;
+    double operandB;
+    switch (c)
+    {
+    case '+':
+        operandA = pop();
+        operandB = pop();
+        double product = operandA + operandB;
+        push(product);
+        break;
+
+    case '-':
+        operandA = pop();
+        operandB = pop();
+        double product = operandB - operandA;
+        push(product);
+        break;
+
+    case '*':
+        operandA = pop();
+        operandB = pop();
+        double product = operandB * operandA;
+        push(product);
+        break;
+
+    case '/':
+        operandA = pop();
+        operandB = pop();
+        double product = operandB / operandA;
+        push(product);
+        break;
+
+    case '=':
+        result = pop();
+        break;
+
+    default:
+        break;
+    }
+}
+
+// checks if c is a numeric character
+int numericCheck(int c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        push(c - '0');
+        return 1;
+    }
+    return 0;
+}
