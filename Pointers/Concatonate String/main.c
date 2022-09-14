3dc3e7b33293a6312958598cc34d7c9946c8ba62
Lemintex <57079368+Lemintex@users.noreply.github.com>
2022-09-14 01:33:41 +0100

    Added Concatenate String With Pointer Magic

---
+++ Pointers/Concatonate String/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MAX_LENGTH 100
+
+void concat(char* s, char* t, char* result);
+
+int main() {
+    char s[] = "hello";
+    char t[] = " world";
+    char result[MAX_LENGTH];
+    concat(s, t, result);
+
+    printf("%s", result);
+}
+
+void concat(char* s, char* t, char* result) {
+    while (*s != '\0') {
+        *result++ = *s++;
+    }
+    while (*t != '\0') {
+        *result++ = *t++;
+    }
+    
+    
+}
