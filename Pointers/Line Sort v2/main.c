8d4a5debd9b342765dbe1d804c23418cc6107b94
Lemintex <57079368+Lemintex@users.noreply.github.com>
2022-09-22 03:18:11 +0100

    Improved Line Sort Using Splicing (WIP)

---
+++ Pointers/Line Sort v2/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LINES 20
+#define MAX_LENGTH 50
+void storeinput(char* input);
+int readlines(const char input[], char* lines[], int sizes[]);
+int getline(const char input[], int* character, char** line, int* size);
+void sortlines(char* lines[], int n);
+void printlines(char* lines[], int sizes[], int n);
+
+// because VScode uses '/n' for end of input, I'll use '|' for line seperator
+int main() {
+    char input[LINES * MAX_LENGTH];
+    char* lines[LINES];
+    int sizes[LINES];
+    int nlines;
+    storeinput(input);
+    if ((nlines = readlines(input, lines, sizes)) > 0) {
+        // sortlines(lines, nlines);
+        printlines(lines, sizes, nlines);
+    }
+}
+
+void storeinput(char* input) {
+    char c;
+    int i;
+    for (i = 0; (c = getchar()) != '\n'; i++) {
+        input[i] = c;
+    }
+    input[i] = '\0';
+}
+
+int readlines(const char input[], char* lines[], int sizes[]) {
+    int character = 0;
+    int n = 0;
+    int lastline;
+    do {
+        lastline = getline(input, &character, &lines[n], &sizes[n]);
+        n++;
+    }
+    while (lastline);
+    return n;
+}
+
+
+int getline(const char input[], int* character, char** start, int* size) {
+    *size = 0;
+    char c;
+    while (input[*character] != '|') {
+        if (*size == 0) {
+            *start = (char*) &input[*character];
+        }
+        *character += 1;
+        if (!input[*character]) return 0;
+        *size += 1;
+    }
+    *character += 1;
+    return 1;
+}
+
+void sortlines(char* lines[], int n) {
+    char* temp;
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            for (int j = i - gap; j >= 0; j -= gap) {
+                if (strcmp(lines[j], lines[j + gap]) <= 0) break;
+                temp = lines[j];
+                lines[j] = lines[j + gap];
+                lines[j + gap] = temp;
+            }
+        }
+    }
+}
+
+void printlines(char* lines[], int sizes[], int n) {
+    for (int i = 0; i <= n; i++) {
+        printf("Line %d: %.*s\n", i+1, sizes[i], lines[i]);
+    }
+}
