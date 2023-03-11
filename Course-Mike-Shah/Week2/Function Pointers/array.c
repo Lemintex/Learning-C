424315e68007a1d8372d593da069b6200c7672c1
Lemintex <aaron_nobile@hotmail.co.uk>
2023-03-11 22:31:01 +0000

    Working on OOP (function pointers) for array struct

---
+++ Course-Mike-Shah/Week2/Function Pointers/array.c
@@ -0,0 +1,22 @@
+#include "array.h"
+
+int Get_array_t(array_t *self, size_t index) {
+	return self->data[index];
+}
+
+void Set_array_t(array_t* self, size_t index, int value) {
+	self->data[index] = value;
+}
+
+
+array_t* make_array(size_t capacity) {
+	array_t* newArray = (array_t*)malloc(sizeof(array_t));
+	newArray->self = newArray;
+	newArray->data = (int*)malloc(sizeof(int)*capacity);
+	newArray->size = capacity;
+
+	newArray->pfnGet = Get_array_t;
+	newArray->pfnSet = Set_array_t;
+
+	return newArray;
+}
---
+++ Course-Mike-Shah/Week2/Function Pointers/array.h
@@ -0,0 +1,21 @@
+#ifndef _ARRAY_H_
+#define  _ARRAY_H_
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct array {
+	struct array* self;
+
+	int* data;
+	size_t size;
+	
+	void (*pfnSet)(struct array*, size_t, int);
+	int (*pfnGet)(struct array*, size_t);
+}array_t;
+
+int Get_array_t(array_t* self, size_t index);
+
+void Set_array_t(array_t* self, size_t index, int value);
+
+array_t* make_array(size_t capacity);
+#endif
